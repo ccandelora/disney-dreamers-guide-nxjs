ddc995a49f4343fa70db2467948244c0df7967b6
Chris Candelora <chris@inigus.com>
2023-08-21 13:16:42 -0400

    about wait time

--- app/magic-kingdom-wait-times/page.jsx
+++ app/magic-kingdom-wait-times/page.jsx
@@ -11,7 +11,7 @@ import photo from "../../public/magic-kingdom.jpg";
 async function getData() {
   const domain = process.env.API_DOMAIN;
   const api = domain + "/api/magic-kingdom-wait-times";
-  const res = await fetch(api, { cache: "no-cache" });
+  const res = await fetch(api);
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
 

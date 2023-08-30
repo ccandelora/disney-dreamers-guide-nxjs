4209c9d01580b740273ba50e3ee3914a38a690b2
Chris Candelora <chris.candelora@gmail.com>
2023-08-30 14:36:18 -0400

    updating ga code

--- app/layout.js
+++ app/layout.js
@@ -14,19 +14,21 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Script
-      async
-      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2603238801904684"
-      crossorigin="anonymous"
-    ></Script>
-      <Script id="google-tag-manager" strategy="afterInteractive">
-        {`
-        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','${GTM_ID}');
-        `}
-    </Script>
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-J1ZE0F4796"
+      />
+      <Script>
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-J1ZE0F4796');
+      </Script>
+
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2603238801904684"
+        crossorigin="anonymous"
+      />
+
       <body className={inter.className}>
         {children}
         <noscript

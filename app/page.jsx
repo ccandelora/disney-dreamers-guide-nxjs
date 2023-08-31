ab3144bd3cc6b1bb51251ad23c7415f98f2e05a5
Chris Candelora <chris.candelora@gmail.com>
2023-08-31 14:44:38 -0400

    removing cache from home page

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import Footer from '../components/Footer'
 
 async function getData() {
   const domain = process.env.API_DOMAIN
-  const res = await fetch(domain + "/api/post",{ next: { revalidate: 900 } })
+  const res = await fetch(domain + "/api/post", { cache: 'no-cache'})
  
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary

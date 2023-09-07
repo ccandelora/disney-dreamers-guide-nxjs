0cab69ca001ac9d198abfe77e3f5bb98b6d9c422
Chris Candelora <chris.candelora@gmail.com>
2023-09-07 18:06:26 -0400

    update to home path

--- app/page.jsx
+++ app/page.jsx
@@ -1,28 +1,34 @@
-import React from 'react';
-import HomeContent from '../components/HomeContent';
+import React from "react";
+import HomeContent from "../components/HomeContent";
 
-const  getData = async () => {
+export const getStaticParams = async () => {
   const domain = process.env.API_DOMAIN;
-  const res = await fetch(domain + "/api/post", { cache: 'no-cache'});
- 
+  const res = await fetch(domain + "/api/post/").then((res) => res.json());
+  const posts = await res.json();
+  return posts;
+}  
+const getData = async () => {
+  const domain = process.env.API_DOMAIN;
+  const res = await fetch(domain + "/api/post", { cache: "no-cache" });
+
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
-    throw new Error('Failed to fetch data');
+    throw new Error("Failed to fetch data");
   }
- 
+
   return res.json();
-}
+};
 
 export const metadata = {
   title: "Disney Dreamer's Guide",
   description: "A guide to help you plan your next Disney vacation.",
   URL: "https://www.disneydreamersguide.com/",
   type: "website",
-}
+};
 
 export const Home = async () => {
   const posts = await getData();
   return <HomeContent posts={posts} />;
-}
- 
+};
+
 export default Home;
